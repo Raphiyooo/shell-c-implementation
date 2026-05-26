ab1a67c5c01beb6b20565c374dbee79fd781d27b
Raphael Strobl <raphael.strobl@student.tugraz.at>
2026-05-26 18:18:36 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-
+int main(int argc, char *argv[])
+{
   while (1)
   {
     setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
     char buffer[1024];
     fgets(buffer, sizeof(buffer), stdin);
 
     buffer[strlen(buffer) - 1] = '\0';
     printf("%s: command not found\n", buffer);
+    printf("$ exit");
+    break;
   }
   return 0;
 }
